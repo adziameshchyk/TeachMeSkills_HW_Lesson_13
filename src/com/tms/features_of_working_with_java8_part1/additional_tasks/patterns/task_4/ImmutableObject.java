a6b50765b1b0495c878d4f59afbd7e050a93fb0b
Aliaksandr Dziameshchyk <dziameshchyk.java@gmail.com>
2024-04-28 14:13:38 +0200

    added realized pattern "ImmutableObject"

---
+++ src/com/tms/features_of_working_with_java8_part1/additional_tasks/patterns/task_4/ImmutableObject.java
@@ -0,0 +1,38 @@
+package com.tms.features_of_working_with_java8_part1.additional_tasks.patterns.task_4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Task:
+ *
+ * Implement an example of the immutable object pattern (this is what we discussed when we wanted to use our custom object as a key in a hash map).
+ */
+
+public class ImmutableObject {
+
+    private final int intValue;
+    private final String stringValue;
+    private final ArrayList<String> listValue;
+
+    public ImmutableObject(int intValue, String stringValue, List<String> listValue) {
+        this.intValue = intValue;
+        this.stringValue = stringValue;
+        this.listValue = new ArrayList<>();
+        this.listValue.addAll(listValue);
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public List<String> getListValue() {
+        return (List<String>) listValue.clone();
+    }
+}
